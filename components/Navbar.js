0de9fa0f005de671454611a89d0bcc079f1345a1
Aamishsn <108685985+Aamishsn@users.noreply.github.com>
2023-04-07 13:54:41 +0500

    styling of nav bar in process

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,35 +1,38 @@
 import React from "react";
 import "../CSS/nav.css"
 import logo from "../assets/logo.png"
+import { Link } from "react-router-dom";
 
 const Navbar=()=>{
 
 return (
     <>
     <div id="nav_body">
-
+        <div>
         <img id="img_body" src={logo} />
-
+        </div>
+        <div>
         <ul id="nav_body_ul">
             <li>
-                Home
+                <Link to="/" className="link_class">Home</Link>
             </li>
             <li>
-               About
+            <Link to="/" className="link_class">About</Link>
             </li>
             <li>
-                Menu
+            <Link to="/" className="link_class">Menu</Link>
             </li>
             <li>
-                Reservations
+            <Link to="/table" className="link_class">Reservations</Link>
             </li>
             <li>
-                Order Online
+            <Link to="/" className="link_class">Order Online</Link>
             </li>
             <li>
-                Login
+            <Link to="/" className="link_class">Login</Link>
             </li>
         </ul>
+        </div>
         </div>
 
 
