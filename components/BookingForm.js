6724e6dc5d41fff1f573ae79c4a6b939ad6a197a
Aamishsn <108685985+Aamishsn@users.noreply.github.com>
2023-04-14 13:34:21 +0500

    form validation 2

--- components/BookingForm.js
+++ components/BookingForm.js
@@ -2,15 +2,29 @@ import React from "react";
 import "../CSS/table.css";
 import { useState } from "react";
 
+const availableTimes = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+
 const BookingForm = () => {
+  
+  const options = availableTimes.map((items) => {
+    return <option>{items}</option>;
+  });
+
   const [name, setName] = useState("");
+  const [isName, setIsName] = useState(false);
+  const [isNameFocus, setIsNameFocus] = useState(true);
   const [ip_email, setEmail] = useState("");
   const [ip_date, setDate] = useState("");
   const [valDate, setValDate] = useState(false);
+  const [guestNumber, setguestNumber] = useState("");
 
   const onCHange_name = (e) => {
     setName(e.target.value);
-    const isName = e.target.value.length > 3 ? true : false;
+    setIsName(e.target.value.length > 3 ? true : false);
+  };
+
+  const focusName = () => {
+    setIsNameFocus(false);
   };
 
   const onChange_email = (e) => {
@@ -28,6 +42,13 @@ const BookingForm = () => {
     const isDate = Date.parse(e.target.value) > 0 ? true : false;
     setValDate(isDate);
   };
+  const guestHandler = (e) => {
+    setguestNumber(e.target.value);
+    const isGuest = e.target.value > 0 && e.target.value < 11 ? true : false;
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <>
@@ -41,55 +62,70 @@ const BookingForm = () => {
             gap: "20px",
             width: "35vw",
           }}
+          onSubmit={handleSubmit}
         >
           <label htmlFor="res-name" style={{ fontWeight: "bold" }}>
             {" "}
             Name:{" "}
           </label>
-          <input
-            type="text"
-            id="res-name"
-            className="res_ip"
-            onChange={onCHange_name}
-            value={name}
-          />
+
+          <div>
+            <input
+              type="text"
+              id="res-name"
+              className="res_ip"
+              onChange={onCHange_name}
+              onBlur={focusName}
+              value={name}
+            />
+            {!isName && !isNameFocus && (
+              <p className="error_msg"> * Reuired Full Name</p>
+            )}
+          </div>
 
           <label htmlFor="res-email" style={{ fontWeight: "bold" }}>
             {" "}
             Email:{" "}
           </label>
-          <input
-            type="email"
-            id="res-email"
-            className="res_ip"
-            onChange={onChange_email}
-            value={ip_email}
-          />
+
+          <div>
+            <input
+              type="email"
+              id="res-email"
+              className="res_ip"
+              onChange={onChange_email}
+              value={ip_email}
+            />
+            <p className="error_msg"> * Email ID is Invalid</p>
+          </div>
 
           <label htmlFor="res-date" style={{ fontWeight: "bold" }}>
             {" "}
             Date:{" "}
           </label>
-          <input
-            type="date"
-            id="res-date"
-            className="res_ip"
-            onChange={dateChangeHandler}
-          />
+
+          <div>
+            <input
+              type="date"
+              id="res-date"
+              className="res_ip"
+              onChange={dateChangeHandler}
+            />
+            <p className="error_msg"> * Email ID is Invalid</p>
+          </div>
 
           <label htmlFor="res-time" style={{ fontWeight: "bold" }}>
             Choose time:
           </label>
           <select id="res-time " className="res_ip" disabled={!valDate}>
-            <option>17:00</option>
-            <option>18:00</option>
-            <option>19:00</option>
-            <option>20:00</option>
-            <option>21:00</option>
-            <option>22:00</option>
+            {options}
           </select>
 
-          <label htmlFor="guests" style={{ fontWeight: "bold" }}>
+          <label
+            htmlFor="guests"
+            style={{ fontWeight: "bold" }}
+            onChange={guestHandler}
+          >
             Number of guests
           </label>
           <input
